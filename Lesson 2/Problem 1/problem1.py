1bbdbefa61681a45988fc2cb19b4d7ebd8ebdf2c
browngirlangie <44147244+browngirlangie@users.noreply.github.com>
2019-02-04 11:27:13 -0500

    Add files via upload

---
+++ Lesson 2/Problem 1/problem1.py
@@ -0,0 +1,15 @@
+schedule_list = ['english', 'math', 'history', 'science', 'gym', 'health', 'advisory', 'production']
+
+for classes_schedule_list
+	 
+schedule_list.sort()
+print(classes)
+
+print'<ul>'
+
+schedule in schedule_list : schedule
+print('_') + classes 
+
+print '</ul>'
+
+		
